22df8da7cd4c6702de88f3868d323c3ebc4fb18d
Jeannie Panting <jeannie.panting@opc.com.au>
2018-10-10 14:07:44 +1100

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import * as React from 'react';
 import { StyleSheet, Text, View, TextInput } from 'react-native';
 import { Constants } from 'expo';
+import { createStackNavigator,} from 'react-navigation';
 import LoginScreen from './components/LoginScreen';
+import ProfileScreen from './components/ProfileScreen';
 
-export default class App extends React.Component {
+const RootStack = createStackNavigator(
+  {
+    Login: LoginScreen,
+    Profile: ProfileScreen,
+  },
+  {
+    initialRouteName: 'Login',
+  }
+);
+
+
+
+export default class Screen extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <LoginScreen />
-      </View>
+      <RootStack />
     );
   }
 }
