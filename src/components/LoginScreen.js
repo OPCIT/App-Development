26d8a1374ca0cf8abe7d1e3c007fd35d497ee8c6
Jeannie Panting <jeannie.panting@opc.com.au>
2018-10-10 17:17:14 +1100

    Authentication changes

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -1,58 +1,92 @@
 import * as React from 'react';
-import {Text, TextInput, View, Button, Alert } from 'react-native';
+import { Text, TextInput, View, Button, Alert } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 
 export default class LoginScreen extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {user: ''};
-    this.state = {password: ''};
+    this.state = { user: '' };
+    //this.onPressLogin = this.onPressLogin.bind(this);
   }
-  onPressLogin(){
-  	() => this.props.navigation.navigate('Profile')
+
+  onPressLogin() {
+    return fetch('http://intranet.opcit.net.au/oauth2/token', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: 
+          'client_id=example_id&username=jeannie.panting@opc.com.au&password=30S3condstom@rs&grant_type=password',
+      
+    })
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.error_description,
+        }, function(){
+
+        });
+        Alert.alert(responseJson.error_description);
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
-   static navigationOptions = {
-          title: 'Welcome',
-          };
- 
+  static navigationOptions = {
+    title: 'Welcome',
+  };
+
   render() {
     return (
-    
-      <View style={{flex:1, justifyContent:'center', padding: 10}}>
-        
-        <View style={{flexDirection:'row',justifyContent: 'space-between', margin:20}}>
-          <View style={{width:100, paddingTop:12}}>
+      <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            margin: 20,
+          }}>
+          <View style={{ width: 100, paddingTop: 12 }}>
             <Text>User Name</Text>
           </View>
-          <View style={{width:180}}>
+          <View style={{ width: 180 }}>
             <TextInput
-              style={{height: 40}}
+              style={{ height: 40 }}
               placeholder="User Name!"
-              onChangeText={(user) => this.setState({user})}
+              onChangeText={user => this.setState({ user })}
             />
           </View>
         </View>
-        <View style={{flexDirection:'row', justifyContent: 'space-between', margin:20}}>
-          <View style={{width:100, paddingTop:12}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            margin: 20,
+          }}>
+          <View style={{ width: 100, paddingTop: 12 }}>
             <Text>Password</Text>
           </View>
-          <View style={{width:180}}>
+          <View style={{ width: 180 }}>
             <TextInput
-            style={{height: 40}}
-            placeholder="Password!"
-            onChangeText={(password) => this.setState({password})}
-          />
+              style={{ height: 40 }}
+              placeholder="Password!"
+              onChangeText={password => this.setState({ password })}
+            />
           </View>
-          </View>
+        </View>
         <View>
-        <Button
-  		onPress={() => this.props.navigation.navigate('Profile')}
-  		title="Login"
-  		style={{height: 40}}
-  		accessibilityLabel="Login"
-		  />
-	    </View>
-    </View>
+          <Button
+            onPress={() => this.onPressLogin()}
+            title="Login"
+            style={{ height: 40 }}
+            accessibilityLabel="Login"
+          />
+        </View>
+        <View>
+        <Text data={this.state.dataSource} />
+        </View>
+      </View>
     );
   }
 }
